ac3110020d1a3f2218927d952a2e12c8280a9191
subrata-bhunia <eerrcfgy@gmail.com>
2021-04-22 15:24:44 +0530

    Agora Testing Web Using react

---
+++ src/constants/index.js
@@ -0,0 +1,15 @@
+export const rtc = {
+  // For the local client
+  client: null,
+  // For the local audio and video tracks
+  localAudioTrack: null,
+  localVideoTrack: null,
+};
+
+export const options = {
+  // Pass your app ID here
+  appId: "4b4decdea2154c2dab1fcbcdd443b195",
+  // Pass a token if your project enables the App Certificate
+  token:"0064b4decdea2154c2dab1fcbcdd443b195IAD1ChYWNLQKNtbzjRgQsaP24Kc2n/K8p7mpooZDi0+ySBOsjjwAAAAAEAAF/wL54ZSCYAEAAQDilIJg",
+  chanelName:'check'
+};
